826dfc2ab40d63b800d4a5fff22b52952e7f5541
Carlos Torres <cctc2013@gmail.com>
2023-04-03 11:16:46 -0400

    Recreacion del error

--- TestExTest/UnitTest1.cs
+++ TestExTest/UnitTest1.cs
@@ -1,12 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TestEx.Controllers;
+using TestEx.Models;
+using TestEx.Repository;
+
 namespace TestExTest
 {
     public class UnitTest1
     {
+        private Mock<ITodoRepository> _mockRepository = new Mock<ITodoRepository>();
+        private TodosController _todoController;
+        public UnitTest1()
+        {
+            _todoController = new TodosController(_mockRepository.Object);
+        }
         
         [Fact]
-        public void Test1()
+        public async Task Test1()
         {
-           
+            var todoDTO = new TodoDTOCreate()
+            {
+                Name = "Test"
+            };
+
+            var todoEnt = new Todo()
+            {
+                Id = 1,
+                Name = "Test",
+                IsActive = true
+            };
+
+            _mockRepository.Setup(x => x.AddOrUpdateAsync(It.IsAny<Todo>()))
+                .ReturnsAsync(todoEnt);
+
+
+            var response = await _todoController.Post(todoDTO);
+
+            Assert.NotNull(response);
+            Assert.IsType<OkObjectResult>(response.Result);
+            Assert.NotNull(response.Value);
         }
     }
 }
